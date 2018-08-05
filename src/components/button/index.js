9daafb193b71377a8faa0f3d7545411ab6165303
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2018-08-05 23:37:29 +0200

    dodanie nowych prototypów komponetów do wybierania

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { TouchableOpacity, View, Text } from "react-native";
+import React, {Component} from "react";
+import {TouchableOpacity, View, Text} from "react-native";
 
 import Color from "../../Color";
 
@@ -7,21 +7,26 @@ export default class Button extends Component {
   render() {
     return (
       <TouchableOpacity
-        style={{ width: "100%", height: 60 }}
-        onPress={this.props.onPress}
-      >
+        style={{
+        width: "100%",
+        height: 60,
+      }}
+        onPress={this.props.onPress}>
         <View
           style={{
-            borderRadius: 20,
-            width: "90%",
-            height: 60,
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: Color.accentColor,
-            alignSelf: "center"
-          }}
-        >
-          <Text style={{ color: "white", fontSize: 20 }}>
+          borderRadius: 20,
+          width: "90%",
+          height: 60,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: Color.accentColor,
+          alignSelf: "center"
+        }}>
+          <Text
+            style={{
+            color: "white",
+            fontSize: 20
+          }}>
             {this.props.text}
           </Text>
         </View>
--- src/scene/Profile.js
+++ src/scene/Profile.js
@@ -10,9 +10,14 @@ export default class Profile extends Component {
     }
     render() {
         return (
-            <ScrollView style={{
-                flex: 1
+            <ScrollView contentContainerStyle={{
+                flex: 1,
+                justifyContent:'center',
+                alignItems:'center',
+                backgroundColor:'white'
             }}>
+<TouchableOpacity style={{ backgroundColor: '#44ff55', width: "90%", height: 60, elevation: 10, borderRadius: 20,}}>
+</TouchableOpacity>
             </ScrollView>
 
         );
