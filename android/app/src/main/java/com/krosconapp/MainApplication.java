5ab71635240d6a718267a021b02a5819291f8546
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2019-09-07 18:34:46 +0200

    fabric refactor

--- android/app/src/main/java/com/krosconapp/MainApplication.java
+++ android/app/src/main/java/com/krosconapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.smixx.fabric.FabricPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new FabricPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
--- src/ErrorUtil.ts
+++ src/ErrorUtil.ts
@@ -1,14 +1,19 @@
 import { NetInfo } from "react-native";
 import Toast from "react-native-simple-toast";
+import { Crashlytics } from 'react-native-fabric';
 export default class ErroUtil {
     public static async errorService(error: any) {
         const connectionInfo = await NetInfo.getConnectionInfo();
         const internetConnectionStatus = connectionInfo.type !== 'none' && connectionInfo.type !== 'unknown';
         if (!internetConnectionStatus) {
-            Toast.show("Brak połączenia z internetem",Toast.LONG);
+            Toast.show("Brak połączenia z internetem", Toast.LONG);
         }
         else if (error) {
-            Toast.show(`Coś poszło nie tak skontaktuj się z obsługą, bądź administratorem.`,Toast.LONG);
+            Toast.show(`Coś poszło nie tak skontaktuj się z obsługą, bądź administratorem.`, Toast.LONG);
         }
+        Crashlytics.recordError({
+            code: "123",
+            message: JSON.stringify(error),
+        });
     }
 }
