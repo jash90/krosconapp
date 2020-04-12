be41a6540ca9fc458d2be860f9707a38cf04af17
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2020-04-12 15:23:14 +0200

    fix icons

--- App.tsx
+++ App.tsx
@@ -25,7 +25,6 @@ import {
 } from "./src/scenes/index";
 import store from "./src/stores";
 import NavigationService from "./src/NavigationService";
-import Crashes from 'appcenter-crashes';
 
 
 const AppNavigator = createStackNavigator(
@@ -48,18 +47,12 @@ const AppNavigator = createStackNavigator(
     },
     {
         headerMode: "none"
-        //   initialRouteName:"About"
     }
 );
 
 const RootNavigator = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
-
-    componentDidMount = async() =>{
-        await Crashes.setEnabled(true);
-        Crashes.generateTestCrash();
-    }
 
     render() {
         return (
@@ -77,27 +70,3 @@ export default class App extends React.Component {
         );
     }
 }
-
-// export default class App extends Component {
-//   componentWillMount() {
-//     Language.setL(DeviceInfo.getDeviceLocale().substring(0, 2));
-//   }
-//   render() {
-//     // return (
-//     //   <Router>
-//     //     <Stack key="root">
-//     //       <Scene key="Loading" component={LoadingScreen} hideNavBar />
-//     //       <Scene key="List" component={List} hideNavBar />
-//     //       <Scene key="Item" component={Item} hideNavBar />
-//     //       <Scene key="Camera" component={Camera} hideNavBar />
-//     //       <Scene key="AddItem" component={AddItem} hideNavBar />
-//     //       <Scene key="Login" component={Login} hideNavBar />
-//     //       <Scene key="Register" component={Register} hideNavBar />
-//     //       <Scene key="Profile" component={Profile} hideNavBar />
-//     //       <Scene key="BorrowBooks" component={BorrowBooks} hideNavBar />
-//     //       <Scene key="Example" component={Example} hideNavBar />
-//     //     </Stack>
-//     //   </Router>
-//     // );
-//   }
-// }
--- android/app/src/main/java/com/krosconapp/MainApplication.java
+++ android/app/src/main/java/com/krosconapp/MainApplication.java
@@ -2,15 +2,14 @@ package com.krosconapp;
 
 import android.app.Application;
 import android.util.Log;
-import com.crashlytics.android.Crashlytics;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.soloader.SoLoader;
-import io.fabric.sdk.android.Fabric;
 import java.util.List;
 
 
