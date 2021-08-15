da365aea06a2a5d83542e8c9bba88217fc368de4
Bartlomiej Zimny <b.zimny@getprintbox.com>
2021-08-15 16:34:22 +0200

    screenContainer fix

--- src/components/ScreenContainer.tsx
+++ src/components/ScreenContainer.tsx
@@ -1,113 +1,111 @@
-import {Container as NContainer} from "native-base";
-import React, {Component} from "react";
-import {findNodeHandle, ScrollView, StyleSheet, Text, View} from "react-native";
-import {ScreenContainerProps} from "../utils/interfaces";
+import { Container as NContainer } from "native-base";
+import React, { Component } from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+import { ScreenContainerProps } from "../utils/interfaces";
 import Head from "./Head";
 
 interface State {
-    viewRef: any;
+  viewRef: any;
 }
 
 class ScreenContainer extends Component<ScreenContainerProps, State> {
-    public backgroundImage: any;
+  public backgroundImage: any;
 
-    static defaultProps = {
-        back: true,
-        left: false,
-        leftIcon: "",
-        leftPress: () => {
-        },
-        right: false,
-        icon: "",
-        scrollView: false,
-        styleContent: null,
-        text: false,
-        children: null,
-        onPress: () => {
-        }
+  static defaultProps = {
+    back: true,
+    left: false,
+    leftIcon: "",
+    leftPress: () => {},
+    right: false,
+    icon: "",
+    scrollView: false,
+    styleContent: null,
+    text: false,
+    children: null,
+    onPress: () => {}
+  };
+
+  constructor(props: ScreenContainerProps) {
+    super(props);
+    this.state = {
+      viewRef: null
     };
+  }
 
-    constructor(props: ScreenContainerProps) {
-        super(props);
-        this.state = {
-            viewRef: null
-        };
+  render() {
+    return (
+      <NContainer>
+        <Head
+          back={this.props.back == undefined ? true : this.props.back}
+          left={this.props.left}
+          leftIcon={this.props.leftIcon}
+          leftPress={this.props.leftPress}
+          text={this.props.text ? this.props.text : "Kroscon"}
+          right={this.props.right}
+          icon={this.props.icon}
+          onPress={this.props.onPress}
+        />
+        {false && (
+          <LostInternetView>
+            <Text style={{ color: "white" }}>
+              {"Brak połączenia z internetem"}
+            </Text>
+          </LostInternetView>
+        )}
+        {this.renderChildren()}
+      </NContainer>
+    );
+  }
+
+  renderChildren() {
+    let Container;
+
+    if (this.props.scrollView) {
+      Container = ScrollView;
+    } else {
+      Container = View;
     }
 
-    render() {
-        return (
-            <NContainer>
-                <Head
-                    back={this.props.back == undefined ? true : this.props.back}
-                    left={this.props.left}
-                    leftIcon={this.props.leftIcon}
-                    leftPress={this.props.leftPress}
-                    text={
-                        this.props.text
-                            ? this.props.text
-                            : "Kroscon"
-                    }
-                    right={this.props.right}
-                    icon={this.props.icon}
-                    onPress={this.props.onPress}
-                />
-                {false && (
-                    <View
-                        style={{
-                            backgroundColor: "red",
-                            paddingHorizontal: 20,
-                            paddingVertical: 10,
-                            alignItems: "center"
-                        }}>
-                        <Text style={{color: "white"}}>
-                            {"Brak połączenia z internetem"}
-                        </Text>
-                    </View>
-                )}
-                {this.renderChildren()}
-            </NContainer>
-        );
-    }
-
-    renderChildren() {
-        if (this.props.scrollView) {
-            return (
-                <ScrollView
-                    style={
-                        this.props.styleContent
-                            ? this.props.styleContent
-                            : styles.fullStyle
-                    }>
-                    {this.props.children}
-                </ScrollView>
-            );
-        } else {
-            return (
-                <View
-                    style={
-                        this.props.styleContent
-                            ? this.props.styleContent
-                            : styles.fullStyle
-                    }>
-                    {this.props.children}
-                </View>
-            );
-        }
-    }
-
-    imageLoaded() {
-        this.setState({
-            viewRef: findNodeHandle(this.backgroundImage)
-        });
-    }
+    return (
+      <Container styleContent={this.props.styleContent}>
+        {this.props.children}
+      </Container>
+    );
+  }
 }
 
-var styles = StyleSheet.create({
-    fullStyle: {
-        flex: 1,
-        backgroundColor: "red",
-        paddingHorizontal: 20
-    },
+const ScrollView = styled.ScrollView`
+  ${props =>
+    !props.styleContent &&
+    `
+    display:flex;
+    flex: 1;
+    background-color: #ccc;
+    padding: 0px 20px;
+  `}
+
+  ${props => !!props.styleContent && props.styleContent}
+`;
+
+const View = styled.View`
+  ${props =>
+    !props.styleContent &&
+    `
+    display:flex;
+    flex: 1;
+    background-color: #ccc;
+    padding: 0px 20px;
+  `}
+
+  ${props => !!props.styleContent && props.styleContent}
+`;
+
+const LostInternetView = styled.View({
+  backgroundColor: "red",
+  paddingHorizontal: 20,
+  paddingVertical: 10,
+  alignItems: "center"
 });
 
 export default ScreenContainer;
